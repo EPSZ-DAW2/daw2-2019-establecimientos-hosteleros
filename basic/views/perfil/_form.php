ae12bbb2e698ab1817f0e28600db4aedb8dc5168
joseordas <joseordas@usal.es>
2019-12-23 22:57:47 +0100

    cambios grupo 4 borrado usarios cread perfil

---
+++ basic/views/perfil/_form.php
@@ -0,0 +1,51 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\perfil */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="perfil-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'nick')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'apellidos')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'fecha_nacimiento')->textInput() ?>
+
+    <?= $form->field($model, 'direccion')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'zona_id')->textInput() ?>
+
+    <?= $form->field($model, 'fecha_registro')->textInput() ?>
+
+    <?= $form->field($model, 'confirmado')->textInput() ?>
+
+    <?= $form->field($model, 'fecha_acceso')->textInput() ?>
+
+    <?= $form->field($model, 'num_accesos')->textInput() ?>
+
+    <?= $form->field($model, 'bloqueado')->textInput() ?>
+
+    <?= $form->field($model, 'fecha_bloqueo')->textInput() ?>
+
+    <?= $form->field($model, 'notas_bloqueo')->textarea(['rows' => 6]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ basic/views/perfil/index.php
@@ -0,0 +1,51 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\perfilSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Perfils';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="perfil-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Perfil', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'email:email',
+            'password',
+            'nick',
+            'nombre',
+            //'apellidos',
+            //'fecha_nacimiento',
+            //'direccion:ntext',
+            //'zona_id',
+            //'fecha_registro',
+            //'confirmado',
+            //'fecha_acceso',
+            //'num_accesos',
+            //'bloqueado',
+            //'fecha_bloqueo',
+            //'notas_bloqueo:ntext',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+
+</div>
