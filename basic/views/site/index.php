d1be19f511d226fa33f7d3bb86dca7d5fcc6389a
Gemadp <usal1692>
2020-01-07 17:35:17 +0100

    Cambios en el index
    Añadido filtro de búsqueda por etiquetas

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -11,25 +11,43 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-        
-        <h3> Locales </h3>
-        <table class="table table-bordered">
-        <tr>
-            <th>Id</th>
-            <th>Titulo</th>
-            <th>Descripcion</th>
-            <th>Categoria Id</th>
-        </tr>
-        <?php foreach($model as $row): ?>
-        <tr>
-            <td><?= $row->id ?></td>
-            <td><?= $row->titulo ?></td>
-            <td><?= $row->descripcion ?></td>
-            <td><?= $row->categoria_id ?></td>
-        </tr>
-        <?php endforeach ?>
-        </table>
+        <div class="container">
+            <h4> Locales </h4>
+            <table class="table table-bordered">
+                <tr>
+                    <th>Id</th>
+                    <th>Titulo</th>
+                    <th>Descripcion</th>
+                    <th>Categoria Id</th>
+                </tr>
+                <?php foreach($model as $row): ?>
+                    <tr>
+                        <td><?= $row->id ?></td>
+                        <td><?= $row->titulo ?></td>
+                        <td><?= $row->descripcion ?></td>
+                        <td><?= $row->categoria_id ?></td>
+                    </tr>
+                <?php endforeach ?>
+            </table>
+        </div>
   
+        <br><br>
 
-    </div>
+        <center>
+            <h3> Filtros de búsqueda de preferencia </h3>
+        </center>
+
+        <br>
+        <!--aqui iria la FICHA RESUMEN, pinchando en los enlaces -->
+        <div class="container">
+            <h4><strong> Filtrar por etiquetas </strong></h4>
+            <div id="etiquetas">
+                <div class="card-block">
+                    <?php 
+                        echo Yii::$app->view->renderFile('@app/views/etiquetas/busquedaetiquetas.php');
+                    ?>
+                </div>
+            </div>
+        </div>
 </div>
+
