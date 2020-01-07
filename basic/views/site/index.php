5f6ce025361dffb58ceecf5dfe13b88f6feae6b7
ArturoHernandez1 <arturohernandez1@usal.es>
2020-01-07 18:04:43 +0100

    Filtro de establecimientos/Locales y creacion del modelo de datos Hostelero

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -11,25 +11,58 @@ $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
 
-        
-        <h3> Locales </h3>
-        <table class="table table-bordered">
-        <tr>
-            <th>Id</th>
-            <th>Titulo</th>
-            <th>Descripcion</th>
-            <th>Categoria Id</th>
-        </tr>
-        <?php foreach($model as $row): ?>
-        <tr>
-            <td><?= $row->id ?></td>
-            <td><?= $row->titulo ?></td>
-            <td><?= $row->descripcion ?></td>
-            <td><?= $row->categoria_id ?></td>
-        </tr>
-        <?php endforeach ?>
-        </table>
-  
+    <nav class="navbar navbar-light navbar2 navbar-toggleable-xs container" >
+        <h4>Filtros</h4>
+           <?php
+                echo Menu::widget([
+                    'options'=>[
+                        "id" => "nav",
+                        "class" => "nav navbar-nav"
+                    ],
+                    'items'=>[
+                        ['label' => 'Establecimientos', 'url' => ['index', 'tipolocal' => 1], 'options'=>["class" => "nav-item"] ],
+                        ['label' => 'Locales', 'url' => ['index', 'tipolocal' => 2], 'options' =>["class" =>"nav-item"]],
+                        ['label' => 'Todos', 'url' => ['index', 'tipolocal' => 0], 'options'=>["class" => "nav-item"] ],
+                    ],
+                ]);
+           ?>
+    </nav>
+    
+       <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        //'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'titulo:ntext',
+            'descripcion:ntext',
+            'lugar:ntext',
+            //'url:ntext',
+            //'zona_id',
+            'categoria_id',
+            //'imagen_id',
+            //'sumaValores',
+            //'totalVotos',
+            //'hostelero_id',
+            //'prioridad',
+            //'visible',
+            //'terminado',
+            //'fecha_terminacion',
+            //'num_denuncias',
+            //'fecha_denuncia1',
+            //'bloqueado',
+            //'fecha_bloqueo',
+            //'notas_bloqueo:ntext',
+            //'cerrado_comentar',
+            //'cerrado_quedar',
+            //'crea_usuario_id',
+            //'crea_fecha',
+            //'modi_usuario_id',
+            //'modi_fecha',
+            //'notas_admin:ntext',
+        ],
+    ]); ?> 
 
     </div>
 </div>
