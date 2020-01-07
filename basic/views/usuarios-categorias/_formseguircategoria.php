b814441b767681ee51cce02c354312f8b40f2184
Gemadp <usal1692>
2020-01-07 17:05:55 +0100

    Añadidos botones a etiquetas y categorias de usuario + cambios apariencia de ellas

--- basic/views/usuarios-categorias/_formseguircategoria.php
+++ basic/views/usuarios-categorias/_formseguircategoria.php
@@ -19,7 +19,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'fecha_seguimiento')->hiddenInput()->label(false) ?>
 
     <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton(Yii::t('app', 'Guardar'), ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
