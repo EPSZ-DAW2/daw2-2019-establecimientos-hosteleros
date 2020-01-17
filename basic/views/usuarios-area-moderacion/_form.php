57bbb13efd2d1db9620f6e724dc2484c4500f0f0
Victor Fagúndez <vfp1905@usal.es>
2020-01-17 18:35:22 +0100

    Cambios en crear areas de Moderación Ahora al seleccionar el area de moderación se puede seleccionar de una menu desplegable

--- basic/views/usuarios-area-moderacion/_form.php
+++ basic/views/usuarios-area-moderacion/_form.php
@@ -1,25 +1,33 @@
 <?php
 
+use app\models\Zonas;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
 /* @var $this yii\web\View */
 /* @var $model app\models\UsuariosAreaModeracion */
 /* @var $form yii\widgets\ActiveForm */
+//$usuarios      = Usuarios::find()->orderBy('nombre')->asArray()->all();
+//$listaUsuarios = ArrayHelper::map($productTypes, 'id', 'name');
+$zonas      = Zonas::find()->orderBy('nombre')->asArray()->all();
+$listaZonas = ArrayHelper::map($zonas, 'id', 'nombre');
 ?>
 
 <div class="usuarios-area-moderacion-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin();?>
+    <?=//$form->field($model, 'usuario_id')->dropDownList($listaUsuarios);
+$form->field($model, 'usuario_id')->textInput()
+?>
 
-    <?= $form->field($model, 'usuario_id')->textInput() ?>
-
-    <?= $form->field($model, 'zona_id')->textInput() ?>
+    <?=
+$form->field($model, 'zona_id')->dropDownList($listaZonas);
+//$form->field($model, 'zona_id')->textInput()?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?=Html::submitButton('Save', ['class' => 'btn btn-success'])?>
     </div>
 
-    <?php ActiveForm::end(); ?>
+    <?php ActiveForm::end();?>
 
 </div>
