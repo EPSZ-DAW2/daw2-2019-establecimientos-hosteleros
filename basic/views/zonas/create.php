56a1671250057614e9a8a3f211d10076c717552f
Victor Fagúndez <vfp1905@usal.es>
2020-01-22 23:37:15 +0100

    Cambios en crear zonas Se ha añadido un menu desplegable para seleccionar la zona relacionada

--- basic/views/zonas/create.php
+++ basic/views/zonas/create.php
@@ -5,16 +5,17 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Zonas */
 
-$this->title = 'Create Zonas';
+$this->title                   = 'Create Zonas';
 $this->params['breadcrumbs'][] = ['label' => 'Zonas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="zonas-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?=Html::encode($this->title)?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+    <?=$this->render('_form', [
+    'model'      => $model,
+    'listaZonas' => $listaZonas,
+])?>
 
 </div>
