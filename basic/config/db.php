1e05257c6451469a829e96a6d448e75672b12488
LauraCas <lauracd@usal.es>
2020-02-03 18:23:21 +0100

    Cambio nombre BD

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class'    => 'yii\db\Connection',
-    'dsn'      => 'mysql:host=localhost;dbname=daw2_hosteleros',
+    'dsn'      => 'mysql:host=localhost;dbname=daw2_hosteleros_LOCALESYCOMENTARIOS',
     'username' => 'root',
     'password' => '',
     'charset'  => 'utf8',
